268b06c79de063e5d9b2e03e31148795e91b0f6c
PigeonCoding <wassimdzd@gmail.com>
2024-10-29 11:16:57 +0100

    now have a print with warning levels :)

--- nice.h
+++ nice.h
@@ -1,9 +1,33 @@
+#pragma once
 #include <stdlib.h>
+#include <stdio.h>
 
-#define test(f) if (f < 0) {                     \
-          fprintf(stderr, "an error has occured\n");\
-          exit(1);                                  \
+#define test(f) if (f < 0 || f == 0) { \
+          dprintf(P_ERR, "an error has occured\n");     \
+          exit(1);                                      \
         }
+
+#define P_INFO 1
+#define P_WARN 2
+#define P_ERR 3
+#define dprintf(type, ...)                   \
+  switch (type) {                            \
+  default:                                   \
+    printf(__VA_ARGS__);                     \
+    break;                                   \
+  case P_INFO:                               \
+    printf("[INFO]: ");                      \
+    printf(__VA_ARGS__);                     \
+    break;                                   \
+  case P_WARN:                               \
+    printf("[WARNING]: ");                   \
+    printf(__VA_ARGS__) ;                    \
+    break;                                   \
+  case P_ERR:                                \
+    fprintf(stderr, "[ERROR]: ");            \
+    fprintf(stderr, __VA_ARGS__);            \
+    break;                                   \
+  }
 
 float shit_sqrt(float in, float div) {
   float s = in / div;
