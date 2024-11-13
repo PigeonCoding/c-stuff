b32a303080d0fb4c8bf18658780d3b9bfb5db0d6
PigeonCoding <wassimdzd@gmail.com>
2024-11-13 13:55:56 +0100

    added array_length macro

--- nice.h
+++ nice.h
@@ -32,7 +32,8 @@
   
 
 // concatonate 2 array of same type into a new array of same type
-#define array_nsize(a, b) sizeof(a) / sizeof(a[0]) + sizeof(b) / sizeof(b[0])
-#define concat_array(a, b, c) test(sizeof(a[0]) == sizeof(b[0]) & sizeof(a[0]) == sizeof(c[0]) & (sizeof(c) / sizeof(c[0])) >= array_nsize(a, b)); memcpy(&c[0], &a[0], sizeof(a)); memcpy(&c[(sizeof(a) / sizeof(a[0]))], &b[0], sizeof(b));
+#define array_length(a) sizeof(a) / sizeof(a[0])
+#define array_nsize(a, b) array_length(a) + array_length(b)
+#define concat_array(a, b, c) test(sizeof(a[0]) == sizeof(b[0]) & sizeof(a[0]) == sizeof(c[0]) & (array_length(c)) >= array_nsize(a, b)); memcpy(&c[0], &a[0], sizeof(a)); memcpy(&c[(array_length(a))], &b[0], sizeof(b));
 
 
