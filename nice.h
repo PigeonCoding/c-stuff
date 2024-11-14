7bddcf852e249fd569a206c98dcc4658f496461d
PigeonCoding <wassimdzd@gmail.com>
2024-11-14 12:29:05 +0100

    more modularity and foreach loop for arrays

--- nice.h
+++ nice.h
@@ -1,39 +1,54 @@
 #pragma once
-#include <stdlib.h>
-#include <stdio.h>
+
+#ifndef V_MEMCPY
 #include <string.h>
+#define V_MEMCPY memcpy
+#endif
 
-#define test(f) if ((f) < 0 || (f) == 0) {                  \
-    eprintf(P_ERR, "an error has occured\n");               \
-    exit(1);                                                \
-  }                                                         \
+#ifndef V_FPRINTF
+#include <stdio.h>
+#define V_FPRINTF fprintf
+#endif
 
+#ifdef V_FPRINTF
 #define P_INFO 1
 #define P_WARN 2
 #define P_ERR 3
-#define eprintf(type, ...)                   \
-  switch (type) {                            \
-  default:                                   \
-    printf(__VA_ARGS__);                     \
-    break;                                   \
-  case P_INFO:                               \
-    printf("[INFO]: ");                      \
-    printf(__VA_ARGS__);                     \
-    break;                                   \
-  case P_WARN:                               \
-    printf("[WARNING]: ");                   \
-    printf(__VA_ARGS__) ;                    \
-    break;                                   \
-  case P_ERR:                                \
-    fprintf(stderr, "[ERROR]: ");            \
-    fprintf(stderr, __VA_ARGS__);            \
-    break;                                   \
-  }                                          \
-  
+#define eprintf(type, ...)                                                     \
+  switch (type) {                                                              \
+  default:                                                                     \
+    V_FPRINTF(stdout, __VA_ARGS__);                                            \
+    break;                                                                     \
+  case P_INFO:                                                                 \
+    fprintf(stdout, "[INFO]: ");                                               \
+    V_FPRINTF(stdout, __VA_ARGS__);                                            \
+    break;                                                                     \
+  case P_WARN:                                                                 \
+    fprintf(stdout, "[WARNING]: ");                                            \
+    V_FPRINTF(stdout, __VA_ARGS__);                                            \
+    break;                                                                     \
+  case P_ERR:                                                                  \
+    fprintf(stderr, "[ERROR]: ");                                              \
+    fprintf(stderr, __VA_ARGS__);                                              \
+    break;                                                                     \
+  }
+#endif
 
-// concatonate 2 array of same type into a new array of same type
 #define array_length(a) sizeof(a) / sizeof(a[0])
 #define array_nsize(a, b) array_length(a) + array_length(b)
-#define concat_array(a, b, c) test(sizeof(a[0]) == sizeof(b[0]) & sizeof(a[0]) == sizeof(c[0]) & (array_length(c)) >= array_nsize(a, b)); memcpy(&c[0], &a[0], sizeof(a)); memcpy(&c[(array_length(a))], &b[0], sizeof(b));
+#ifdef V_MEMCPY
+// concatonate 2 array of same type into a new array of same type
+#define concat_array(a, b, c)                                                  \
+  test(sizeof(a[0]) == sizeof(b[0]) & sizeof(a[0]) == sizeof(c[0]) &           \
+       (array_length(c)) >= array_nsize(a, b));                                \
+  V_MEMCPY(&c[0], &a[0], sizeof(a));                                           \
+  V_MEMCPY(&c[(array_length(a))], &b[0], sizeof(b));
+#endif // NO_STR
 
-
+#define aforeach_val(type, name, array, i)                                     \
+  for (unsigned long i = 0; i < array_length(array); i++) {                    \
+    type name = array[i];
+#define aforeach_ref(type, name, array, i)                                     \
+  for (unsigned long i = 0; i < array_length(array); i++) {                    \
+    type *name = &array[i];
+#define end_foreach }
